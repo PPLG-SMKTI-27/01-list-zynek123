1d1e423b1aa26cfe1833bc92016fcc8af41dca5f
zynek123 <mamankresingiyeke@gmail.com>
2025-07-21 14:48:22 +0800

    rasya.py

---
+++ rasya.py
@@ -0,0 +1,14 @@
+nilai = [10, 20, 30] 
+[40, 50, 60]
+
+for i in range (len(nilai)):
+    index0 = nilai[0] [0] + nilai [0] [1] + nilai [0] [2]
+    index1 = nilai[1] [0] + nilai [1] [1] + nilai [1] [2]
+    index2 = nilai[2] [0] + nilai [2] [1] + nilai [2] [2]
+
+    print(f""""
+    indek0
+    index1 
+    index2
+    """)
+    
